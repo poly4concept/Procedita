4ab4a32e7afe4e78900624981e0e333a94a05e56
Poly4 <olajuwonmubarak355@gmail.com>
2022-05-11 22:55:56 +0100

    testing the github actions

--- src/components/hero/index.jsx
+++ src/components/hero/index.jsx
@@ -9,12 +9,12 @@ const Hero = () => {
     return (
         <div className='hero'>
             <div className='hero-left'>
-                <h3>We provide a comprehensive  debt collection  service</h3>
+                <h3>We provide a much comprehensive  debt collection  service</h3>
                 <p>Our professionals have a combined 85 years in debt recovery efforts that have seen the recovery of more than N19Billion in debt from very unwilling debtors.
                     We use the most advanced techniques to trace debtors and recover money owed. We also offer an alternative method for loan recoveries by allowing debtors provide value worth the amount they are unable to pay.</p>
                 <div className="hero-left-btns">
                     <Button text='Hire Us' />
-                    <a href='#video' className='btn2'><BsFillPlayFill className='play-icon'/><span>Watch Video</span></a>
+                    <a href='#video' className='btn2'><BsFillPlayFill className='play-icon'/><span>Watch a Video</span></a>
                 </div>
             </div>
             <div className="hero-right">
