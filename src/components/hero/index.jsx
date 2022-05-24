21650de6522eb3839381d35958e1a533678a40a7
Poly4 <olajuwonmubarak355@gmail.com>
2022-05-24 23:41:02 +0100

    made a change to test CI/CD

--- src/components/hero/index.jsx
+++ src/components/hero/index.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
     return (
         <div className='hero'>
             <div className='hero-left'>
-                <h3>We provide a much comprehensive  debt collection  service</h3>
+                <h3>We provide a much comprehensive  debt collection service for our customers</h3>
                 <p>Our professionals have a combined 85 years in debt recovery efforts that have seen the recovery of more than N19Billion in debt from very unwilling debtors.
                     We use the most advanced techniques to trace debtors and recover money owed. We also offer an alternative method for loan recoveries by allowing debtors provide value worth the amount they are unable to pay.</p>
                 <div className="hero-left-btns">
